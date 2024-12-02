4fe3d78f532eaf76ae4975b28628c3573e79dbed
Paweł Szatanik <87148178+Pawcix@users.noreply.github.com>
2024-12-02 19:31:39 +0100

    Add Particle scripts and styling them

--- script.js
+++ script.js
@@ -19,104 +19,108 @@ let heart = 3;
 let second = 0;
 
 const displayCounter = () => {
-    second++;
-    if (timer) {
-        timer.innerHTML = 'Time: ' + second + 's';
-    }
+  second++;
+  if (timer) {
+    timer.innerHTML = 'Time: ' + second + 's';
+  }
 }
 
 const startNewGame = () => {
-    window.location.reload();
+  window.location.reload();
 }
 
 const gameOverModal = () => {
-    if (heart === 0) {
-        gameOver.classList.add('flex')
-        game.classList.add('none')
-    }
+  if (heart === 0) {
+    gameOver.classList.add('flex')
+    game.classList.add('none')
+  }
 }
 
 const generateQuestion = () => {
-    if (answers <= 10) {
-        x = Math.floor(Math.random() * 11);
-        y = Math.floor(Math.random() * 11);
-        level.innerHTML = '🐳 Level: 1';
-        level.style.color = 'blue';
-    } else if (answers <= 20) {
-        x = Math.floor(Math.random() * 21) + 10;
-        y = Math.floor(Math.random() * 21) + 10;
-        level.innerHTML = '🐸 Level: 2';
-        level.style.color = 'green';
-    } else if (answers <= 30) {
-        x = Math.floor(Math.random() * 31) + 20;
-        y = Math.floor(Math.random() * 31) + 20;
-        level.innerHTML = '🐱 Level: 3';
-        level.style.color = 'gold';
-    } else if (answers <= 40) {
-        x = Math.floor(Math.random() * 41) + 30;
-        y = Math.floor(Math.random() * 41) + 30;
-        level.innerHTML = '🦊 Level: 4';
-        level.style.color = 'orange';
-    } else {
-        x = Math.floor(Math.random() * 51) + 40;
-        y = Math.floor(Math.random() * 51) + 40;
-        level.innerHTML = '🐙 Level: 5';
-        level.style.color = 'red'
-    }
+  if (answers <= 10) {
+    x = Math.floor(Math.random() * 11);
+    y = Math.floor(Math.random() * 11);
+    level.innerHTML = '🐳 Level: 1';
+    level.style.color = 'blue';
+  } else if (answers <= 20) {
+    x = Math.floor(Math.random() * 21) + 10;
+    y = Math.floor(Math.random() * 21) + 10;
+    level.innerHTML = '🐸 Level: 2';
+    level.style.color = 'green';
+  } else if (answers <= 30) {
+    x = Math.floor(Math.random() * 31) + 20;
+    y = Math.floor(Math.random() * 31) + 20;
+    level.innerHTML = '🐱 Level: 3';
+    level.style.color = 'gold';
+  } else if (answers <= 40) {
+    x = Math.floor(Math.random() * 41) + 30;
+    y = Math.floor(Math.random() * 41) + 30;
+    level.innerHTML = '🦊 Level: 4';
+    level.style.color = 'orange';
+  } else {
+    x = Math.floor(Math.random() * 51) + 40;
+    y = Math.floor(Math.random() * 51) + 40;
+    level.innerHTML = '🐙 Level: 5';
+    level.style.color = 'red'
+  }
 
-    if (answers === 1) {
-        setInterval(function () {
-            displayCounter();
-        }, 1000)
-    }
+  if (answers === 1) {
+    setInterval(function () {
+      displayCounter();
+    }, 1000)
+  }
 
-    v = x + y;
+  v = x + y;
 
-    numOne.innerHTML = x;
-    numTwo.innerHTML = y;
-    inputValue.value = '';
-    liveHeart.innerHTML = "Live: " + heart;
+  numOne.innerHTML = x;
+  numTwo.innerHTML = y;
+  inputValue.value = '';
+  liveHeart.innerHTML = "Live: " + heart;
 }
 
 const checkAnswers = () => {
-    const userInput = parseInt(inputValue.value, 10)
+  const userInput = parseInt(inputValue.value, 10)
 
-    if (userInput === v) {
-        answers++;
-        goodAnswersCounter.innerHTML = "Correct Answers: " + answers;
-        goodAnswersCounterInGameOver.innerHTML = "Correct Answers: " + answers;
-        goodAnswers.style.display = 'flex'
-        badAnswers.style.display = 'none'
-        inputValue.classList.add('good-border');
-        generateQuestion();
+  if (userInput === v) {
+    answers++;
+    goodAnswersCounter.innerHTML = "Correct Answers: " + answers;
+    goodAnswersCounterInGameOver.innerHTML = "Correct Answers: " + answers;
+    goodAnswers.style.display = 'flex'
+    badAnswers.style.display = 'none'
+    inputValue.classList.add('good-border');
+    generateQuestion();
 
-        setTimeout(() => {
-            inputValue.classList.remove('good-border'),
-                goodAnswers.style.display = 'none'
-        }, 2000)
-    } else {
-        heart--;
-        inputValue.value = '';
-        liveHeart.innerHTML = "Live: " + heart;
+    setTimeout(() => {
+      inputValue.classList.remove('good-border'),
         goodAnswers.style.display = 'none'
-        badAnswers.style.display = 'flex'
-        inputValue.classList.add('bad-border');
-        gameOverModal();
+    }, 2000)
+  } else {
+    heart--;
+    inputValue.value = '';
+    liveHeart.innerHTML = "Live: " + heart;
+    goodAnswers.style.display = 'none'
+    badAnswers.style.display = 'flex'
+    inputValue.classList.add('bad-border');
+    gameOverModal();
 
-        setTimeout(() => {
-            inputValue.classList.remove('bad-border'),
-                badAnswers.style.display = 'none'
-        }, 2000)
-    }
+    setTimeout(() => {
+      inputValue.classList.remove('bad-border'),
+        badAnswers.style.display = 'none'
+    }, 2000)
+  }
 }
 
 const checkEnter = (event) => {
-    if (event.key === "Enter") {
-        checkAnswers();
-    }
+  if (event.key === "Enter") {
+    checkAnswers();
+  }
 }
 
 generateQuestion();
 startGame.addEventListener('click', startNewGame);
 checkButton.addEventListener('click', checkAnswers);
 inputValue.addEventListener('keypress', checkEnter);
+
+particlesJS.load('particles-js', 'particles.json', function () {
+  console.log('Particles.js config loaded');
+});
